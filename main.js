087ac8667b5db0ee974010efd56a5e78c692f979
Vladimir (vapronva) <git@vprw.ru>
2023-09-17 00:40:31 +0300

    add: preset, tune, profile options; change: crf via input value

--- main.js
+++ main.js
@@ -1,75 +1,131 @@
-async function register ({
+async function register({
     registerSetting,
     settingsManager,
     transcodingManager
-  }) {
-    const defaultCRF = 20
-  
+}) {
+    const defaultCRF = 23
+    const defaultPreset = "medium"
+    const defaultTune = null
+    const defaultProfile = null
+
     const store = {
-      crf: await settingsManager.getSetting('crf') || defaultCRF
+        crf: await settingsManager.getSetting("crf") || defaultCRF,
+        preset: await settingsManager.getSetting("preset") || defaultPreset,
+        tune: await settingsManager.getSetting("tune") || defaultTune,
+        profile: await settingsManager.getSetting("profile") || defaultProfile
     }
-  
     settingsManager.onSettingsChange(settings => {
-      store.crf = settings['crf']
+        store.crf = settings["crf"]
+        store.preset = settings["preset"]
+        store.tune = settings["tune"]
+        store.profile = settings["profile"]
     })
-  
-    const builderVOD = (options) => {
-      return {
-        outputOptions: [
-          `-r ${options.fps}`,
-          `-crf ${store.crf}`
-        ]
-      }
+
+    const buildVOD = (options) => {
+        return {
+            outputOptions: [
+                `-r ${options.fps}`,
+                `-crf ${store.crf}`,
+                `-preset ${store.preset}`,
+                store.tune ? `-tune ${store.tune}` : "",
+                store.profile ? `-profile:v ${store.profile}` : ""
+            ]
+        }
     }
-  
+
     const buildLive = (options) => {
-      return {
-        outputOptions: [
-          `${buildStreamSuffix('-r:v', options.streamNum)} ${options.fps}`,
-          `-crf ${store.crf}`
-        ]
-      }
+        return {
+            outputOptions: [
+                `${buildStreamSuffix("-r:v", options.streamNum)} ${options.fps}`,
+                `-crf ${store.crf}`
+            ]
+        }
     }
-  
+
     registerSetting({
-      name: 'crf',
-      label: 'Quality',
-      type: 'select',
-      options: [
-        { label: 'Default', value: 23 },
-        { label: 'Good', value: 20 },
-        { label: 'Very good', value: 17 },
-        { label: 'Excellent', value: 14 }
-      ],
-      descriptionHTML: 'Increasing quality will result in bigger video sizes',
-      private: true,
-      default: defaultCRF
+        name: "crf",
+        label: "Constatnt Rate Factor (CRF)",
+        type: "input",
+        descriptionHTML: "Set the quality for the encode. Lower values mean better quality, but larger file sizes. The default is 23, and sane values are between 18 and 28. The range is logarithmic, so increasing the CRF by 6 roughly doubles the file size, while decreasing it by 6 roughly halves the file size. Available values are 0-51.",
+        private: true,
+        default: defaultCRF
     })
-  
-    const encoder = 'libx264'
-    const profileName = 'custom-quality'
-  
-    transcodingManager.addVODProfile(encoder, profileName, builderVOD)
+
+    registerSetting({
+        name: "preset",
+        label: "Preset",
+        type: "select",
+        options: [
+            { label: "Ultra Fast", value: "ultrafast" },
+            { label: "Super Fast", value: "superfast" },
+            { label: "Very Fast", value: "veryfast" },
+            { label: "Faster", value: "faster" },
+            { label: "Fast", value: "fast" },
+            { label: "Medium", value: "medium" },
+            { label: "Slow", value: "slow" },
+            { label: "Slower", value: "slower" },
+            { label: "Very Slow", value: "veryslow" }
+        ],
+        descriptionHTML: "Set the preset to be used when encoding. A slower preset will provide better compression (compression is quality per filesize). This means that, for example, if you target a certain file size or constant bit rate, you will achieve better quality with a slower preset. Similarly, for constant quality encoding, you will simply save bitrate by choosing a slower preset.",
+        private: true,
+        default: defaultPreset
+    })
+
+    registerSetting({
+        name: "tune",
+        label: "Tune",
+        type: "select",
+        options: [
+            { label: "None", value: null },
+            { label: "Film", value: "film" },
+            { label: "Animation", value: "animation" },
+            { label: "Grain", value: "grain" },
+            { label: "Still Image", value: "stillimage" },
+            { label: "Fast Decode", value: "fastdecode" },
+            { label: "Zero Latency", value: "zerolatency" },
+        ],
+        descriptionHTML: "Tune the settings for a particular type of source or situation. If you are unsure, leave this as None.",
+        private: true,
+        default: defaultTune
+    })
+
+    registerSetting({
+        name: "profile",
+        label: "Profile",
+        type: "select",
+        options: [
+            { label: "None", value: null },
+            { label: "Baseline", value: "baseline" },
+            { label: "Main", value: "main" },
+            { label: "High", value: "high" },
+            { label: "High 10", value: "high10" },
+            { label: "High 4:2:2", value: "high422" },
+            { label: "High 4:4:4", value: "high444" },
+        ],
+        descriptionHTML: "Set the profile to be used when encoding. Note that some profiles are not supported by some codecs, and not all devices support all profiles. If you are unsure, leave this as None.",
+        private: true,
+        default: defaultProfile
+    })
+
+    const encoder = "libx264"
+    const profileName = "vprw-custom-profile"
+
+    transcodingManager.addVODProfile(encoder, profileName, buildVOD)
     transcodingManager.addLiveProfile(encoder, profileName, buildLive)
-  }
-  
-  async function unregister () {
+}
+
+async function unregister() {
     return
-  }
-  
-  module.exports = {
+}
+
+module.exports = {
     register,
     unregister
-  }
-  
-  
-  // ---------------------------------------------------------------------------
-  
-  function buildStreamSuffix (base, streamNum) {
+}
+
+function buildStreamSuffix(base, streamNum) {
     if (streamNum !== undefined) {
-      return `${base}:${streamNum}`
+        return `${base}:${streamNum}`
     }
-  
     return base
-  }
-  
+}
